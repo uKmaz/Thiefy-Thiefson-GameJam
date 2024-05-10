c3ae67118d27b6ee76f898d1c794eb9d17f3e87a
Emre Uçmaz <157684522+uKmaz@users.noreply.github.com>
2024-05-11 01:11:00 +0300

    New project created and deployed.

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class TreasureSpawner : MonoBehaviour
+{
+    public int numberOfTreasures = 5;
+    public GameObject treasurePrefab;
+    public float spawnRadius = 5f;
+    public float leftBorder = -3f;
+    public float rightBorder = 13f;
+    public float upBorder = 9f;
+    public float downBorder = 0f;
+
+    void Start()
+    {
+        SpawnTreasures();
+    }
+
+    void SpawnTreasures()
+    {
+        for (int i = 0; i < numberOfTreasures; i++)
+        {
+            Vector2 randomPosition = GetRandomPositionWithinBorders();
+
+            Instantiate(treasurePrefab, randomPosition, Quaternion.identity);
+        }
+    }
+    Vector2 GetRandomPositionWithinBorders()
+    {
+        float randomX = Random.Range(leftBorder, rightBorder);
+        float randomY = Random.Range(downBorder, upBorder);
+
+        return new Vector2(randomX, randomY);
+    }
+}
